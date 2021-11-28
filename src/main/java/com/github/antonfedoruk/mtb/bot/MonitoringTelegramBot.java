b395634a280817f0eeb852baa3c99fe711e538da
Anton <antobn.fedoruk@gmail.com>
2021-11-28 15:15:39 +0200

    MTB-1: Add stub telegram bot to the project.

---
+++ src/main/java/com/github/antonfedoruk/mtb/bot/MonitoringTelegramBot.java
@@ -0,0 +1,51 @@
+package com.github.antonfedoruk.mtb.bot;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Telegram bot for sites monitoring.
+ */
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class MonitoringTelegramBot extends TelegramLongPollingBot {
+    @Value("${telegrambot.botUserName}")
+    String botUserName;
+    @Value("${telegrambot.botToken}")
+    String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+            SendMessage sm = new SendMessage();
+            sm.setChatId(chatId);
+            sm.setText(message);
+
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                //todo add logging to the project.
+                e.printStackTrace();
+            }
+        }
+    }
+}
